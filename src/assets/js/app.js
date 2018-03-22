347d473f1245ba53600dc58afe434c23e13e33b5
Martinez <eamartinez@wt107271.wtacademic.wtamu.edu>
2018-03-22 14:28:07 -0500

    updated youtube code to be accessible and runs on latest jquery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -117,36 +117,26 @@ $(document).ready(function () {
     }
 
     if($('.youtube').length){
-        ( function() {
 
-            var youtube = document.querySelectorAll( ".youtube" );
-            
-            for (var i = 0; i < youtube.length; i++) {
-                
-                var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
-                
-                var image = new Image();
-                        image.src = source;
-                        image.alt = youtube[i].dataset.alt
-                        image.addEventListener( "load", function() {
-                            youtube[ i ].appendChild( image );
-                        }( i ) );
-                
-                        youtube[i].addEventListener( "click", function() {
-
-                            var iframe = document.createElement( "iframe" );
-
-                                iframe.setAttribute( "frameborder", "0" );
-                                iframe.setAttribute( "allowfullscreen", "" );
-                                iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
-
-                                this.innerHTML = "";
-                                this.appendChild( iframe );
-                        } );    
-                    };                
-                } 
-            )
-        ();
+        $.each(
+            $('.youtube'), function(index, value) {                
+                console.log($(this));
+                $(this).css("background","url('https://img.youtube.com/vi/" + $(this).attr("data-embed") + "/sddefault.jpg') no-repeat");
+                $(this).css("background-position","center");
+                $(this).css("background-size","cover");
+                $(this).click(function(){
+                    $(this).find(".play-button").remove();
+                    var iframe = document.createElement( "iframe" );
+                    iframe.setAttribute( "frameborder", "0" );
+                    iframe.setAttribute( "allowfullscreen", "" );
+                    iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ $(this).attr("data-embed") +"?rel=0&showinfo=0&autoplay=1" );
+                    $(this).innerHTML = $(this).attr("data-alt");
+                    $(this).append(iframe); 
+                    return false;                    
+                });
+            }
+        ); 
+        
         console.log("Youtube class detected : youtube script Loaded");
     }else{
         console.log("No Youtube class detected:" + $('.youtube').length);
